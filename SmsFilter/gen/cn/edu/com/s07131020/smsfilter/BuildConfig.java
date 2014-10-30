417b7c77a631a9dfda409ac9e46185af08fa0451
gdmec07131020 <1063355293@qq.com>
2014-10-30 23:01:09 +0800

    SmsFilter

---
+++ SmsFilter/gen/cn/edu/com/s07131020/smsfilter/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.edu.com.s07131020.smsfilter;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SmsFilter/src/cn/edu/com/s07131020/smsfilter/Receiver.java
@@ -0,0 +1,30 @@
+  package cn.edu.com.s07131020.smsfilter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class Receiver extends BroadcastReceiver {
+
+	public void onReceive(Context context, Intent intent) {  
+		
+		Bundle bundle = intent.getExtras();   
+	   
+        Object[] object = (Object[])bundle.get("pdus");   
+     
+        SmsMessage sms[]=new SmsMessage[object.length];   
+        for(int i=0;i<object.length;i++)   
+        {   
+            sms[0] = SmsMessage.createFromPdu((byte[])object[i]);  
+			
+    		if(sms[i].getDisplayOriginatingAddress().toString().equals("110")){
+                Toast.makeText(context, "号码："+sms[i].getDisplayOriginatingAddress()+" 发来的短信已被拦截，内容是："+sms[i].getDisplayMessageBody(), Toast.LENGTH_SHORT).show();   
+    			abortBroadcast();                                  
+    		};
+        }   
+ 	}  
+
+}
